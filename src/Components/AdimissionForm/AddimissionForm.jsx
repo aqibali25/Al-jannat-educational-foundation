8339680a607c462c6c4cbbd4d8011701b6c09eda
aqibali25 <aqibalikalwar1@gmail.com>
2024-09-19 16:34:30 +0500

    Added Admission Form

---
+++ src/Components/AdimissionForm/AddimissionForm.jsx
@@ -0,0 +1,11 @@
+const AdmissionForm = () => {
+  return (
+    <div>
+      <h1>Admission Page</h1>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam
+      voluptatem autem voluptates in deleniti hic illo non porro laborum, at
+      nesciunt facilis vitae quae repellat iusto recusandae aperiam, ullam sit?
+    </div>
+  );
+};
+export default AdmissionForm;
