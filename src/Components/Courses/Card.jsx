c79857cbaafe06a5bb67c3979bd98ba499b549a9
aqibali25 <aqibalikalwar1@gmail.com>
2024-10-03 01:06:11 +0500

    Added Enroll And Advertisement Button

--- src/Components/Courses/Card.jsx
+++ src/Components/Courses/Card.jsx
@@ -1,14 +1,24 @@
 const Card = ({ course, onClick }) => {
   return (
-    <div className="col-md-3 courseCard" onClick={onClick}>
+    <div className="col-md-3 courseCard">
       <div className="card mb-4 shadow-sm">
-        <div className="availableBox">
+        <div
+          className={`availableBox ${
+            course.available ? "isAvailable" : "notAcailable"
+          }`}
+        >
           <span>{course.available ? "Available" : "Available Soon"}</span>
         </div>
         <img src={course.imgSrc} className="card-img-top" alt={course.title} />
         <div className="card-body">
           <h5 className="card-title">{course.title}</h5>
           <p className="card-text">{course.description}</p>
+        </div>
+        <div className="CoursesButtons">
+          <button className="btn btn-primary" onClick={onClick}>
+            Enroll Now
+          </button>
+          <button className="btn btn-info">Advertisement</button>
         </div>
       </div>
     </div>
